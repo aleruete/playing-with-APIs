ba706f8d351a00a2ff5560b11e1dd3efa0120995
aleruete <aleruete@gmail.com>
2020-05-08 09:32:03 +0200

    update

--- nbn4r.R
+++ nbn4r.R
@@ -3,3 +3,5 @@
 remotes::install_github("fozy81/NBN4R")
 library(NBN4R)
 nbn_config(cache_directory="C:/Users/Alejandro/Documents/GitHub/playing-with-APIs/nbn_cache") ## Windows
+x <- occurrences(taxon="taxon_name:\"Accipiter gentilis\"", download_reason_id="testing", email="test@test.org")
+summary(x)
